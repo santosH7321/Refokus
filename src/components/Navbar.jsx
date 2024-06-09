1eb8f0b208ef554f76a94e778e9f45dd4b253dd3
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-06-09 06:51:00 +0530

    costButton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,32 @@
-
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <div className="max-w-screen-xl mx-auto py-6 flex items-center">
-        <img src="https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
-        <div className="links flex gap-10 ml-20">
-            {["Home", "Work", "About","", "News"].map((elem, index) => (
-                elem.length === 0 ? <span className="w-[2px] h-7 bg-zinc-700"></span> : (<a key={index} className="font-regular text-sm flex items-center gap-1" href="#">
-                {index === 1 && (
-                  <span
-                    style={{ boxShadow: "0 0 0.45em #00FF19" }}
-                    className="inline-block w-2 h-2 rounded-full bg-green-500"
-                  >
-                    
-                  </span>
-                )}
-                {elem}
-              </a>)
-              
-            ))}
-        </div>
+    <div className="max-w-screen-xl mx-auto py-6 flex items-center justify-between border-b-[1px] border-zinc-700">
+      <div className="nleft flex items-center">
+      <img src="https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
+      <div className="links flex gap-10 ml-20">
+        {["Home", "Work", "About", "", "News"].map((elem, index) => (
+          elem.length === 0 ? (
+            <span key={index} className="w-[2px] h-7 bg-zinc-700"></span>
+          ) : (
+            <a key={index} className="font-regular text-sm flex items-center gap-1" href="#">
+              {index === 1 && (
+                <span
+                  style={{ boxShadow: "0 0 0.45em #00FF19" }}
+                  className="inline-block w-2 h-2 rounded-full bg-green-500"
+                ></span>
+              )}
+              {elem}
+            </a>
+          )
+        ))}
+      </div>
+      </div>
+
+      <Button />
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
